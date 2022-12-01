369ffffb4dd0bf47460791800075cc1ece22b568
Tyler Morley <TylerPMorley@gmail.com>
2022-12-01 11:35:42 -0600

    Day01 streamlined code after looking at others' solutions

--- 2022/code/day01.rb
+++ 2022/code/day01.rb
@@ -2,57 +2,37 @@
 
 class Day01
   def get_elves (filename)
+    lines = File.read(filename).to_s
+    elves_str = lines.split("\n\n").map{|elf| elf.split("\n")}
     elves = Array.new(0)
-    elf = Array.new(0)
 
-    File.readlines(filename).each do |line|
-      line = line.strip
-      if line != ""
-        elf.append(line.to_i)
-      else
-        elves.append(elf)
-        elf = Array.new(0)
-      end
+    for elf_str in elves_str
+      elf = elf_str.map{|food_str| food_str.to_i}
+
+      elves.append(elf)
     end
-    elves.append(elf)
 
     return elves
   end
 
   def get_calories (elves)
-    elves_calories = Array.new(0)
-
-    for i in 0..elves.length-1
-      elf_calories = 0
-
-      for j in 0..elves[i].length-1
-        elf_calories += elves[i][j]
-      end
-
-      elves_calories.append(elf_calories)
-    end
-    
-    return elves_calories
+    calory_totals = elves.map{|elf| elf.sum}
+    return calory_totals
   end
 
   def get_calories_top1 (calories_list)
-    calories_list.max
+    return calories_list.max
   end
 
   def get_calories_top3 (calories_list)
-    top_three = []
-    for i in 0..2
-      top_elf = calories_list.max
-      top_three.append(top_elf)
-      calories_list.delete(top_elf)
-    end
+    top_three = calories_list.sort[-3..]
     return top_three.sum
   end
 end
 
 if __FILE__ == $0    
   day_one = Day01.new
-  input = day_one.get_elves('inputs/input01.txt')
+  input = day_one.get_elves('inputs/realinput01.txt')
   calories_list = day_one.get_calories(input)
 
   part1 = day_one.get_calories_top1(calories_list)
--- 2022/tests/test01.rb
+++ 2022/tests/test01.rb
@@ -4,23 +4,25 @@ require 'code/day01'
 class Part1 < Test::Unit::TestCase
   def test_get_elves
     day_one = Day01.new
+
     elves = day_one.get_elves('inputs/testinput01.txt')
     
-    expected_len = 5
-    assert_equal expected_len, elves.length
-
-    assert_equal 3, elves[0].length
-    assert_equal 2, elves[2].length
+    exp_length = 5
+    assert_equal exp_length, elves.length
+    expected = [[1000, 2000, 3000], [4000], [5000, 6000], [7000, 8000, 9000], [10000]]
+    assert_equal expected, elves
   end
 
   def test_get_calories
+    day_one = Day01.new
     elves = [[1000, 2000, 3000], [4000], [5000, 6000], [7000, 8000, 9000], [10000]]
 
-    day_one = Day01.new
     calories = day_one.get_calories(elves)
 
     exp_length = 5
     assert_equal exp_length, calories.length
+    expected = [6000, 4000, 11000, 24000, 10000]
+    assert_equal expected, calories
   end
 
   def test_get_calories_top1
@@ -36,8 +38,8 @@ end
 
 class Part2 < Test::Unit::TestCase
   def test_get_calories_top3
-    input_calories = [6000, 4000, 11000, 24000, 10000]
     day_one = Day01.new
+    input_calories = [6000, 4000, 11000, 24000, 10000]
 
     top_three = day_one.get_calories_top3(input_calories)
 
