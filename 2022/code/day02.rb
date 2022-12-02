dce905fdf7eb7542fbee28bcce212db412a53818
Tyler Morley <TylerPMorley@gmail.com>
2022-12-02 13:29:29 -0600

    day02 part 2

--- 2022/code/day02.rb
+++ 2022/code/day02.rb
@@ -8,34 +8,63 @@ class Day02
     return guide
   end
 
-  def decode (column)
+  def decode_1 (column)
     codes = {'A':'rock', 'B':'paper', 'C':'scissors', 'X':'rock', 'Y':'paper', 'Z':'scissors'}
     return codes[column.to_sym]
   end
 
-  def apply_rules (my_choice, opponent_choice)
-    rules = {'rock': {'rock':'tie', 'paper': 'lose', 'scissors':'win'}, 'paper': {'rock':'win', 'paper':'tie', 'scissors':'lose'}, 'scissors': {'rock':'lose', 'paper':'win', 'scissors':'tie'}}
-    
-    return rules[my_choice.to_sym][opponent_choice.to_sym]
+  def decode_2 (column)
+    codes = {'A':'rock', 'B':'paper', 'C':'scissors', 'X':'lose', 'Y':'tie', 'Z':'win'}
+    return codes[column.to_sym]
   end
 
-  def calculate_round (round)
+  def apply_rules (my_choice, opponent_choice, mode)
+    rules = {'rock': {'rock':'tie', 'paper': 'win', 'scissors':'lose'}, 'paper': {'rock':'lose', 'paper':'tie', 'scissors':'win'}, 'scissors': {'rock':'win', 'paper':'lose', 'scissors':'tie'}}
+    
+    output = nil
+
+    if mode == '1'
+      output = rules[opponent_choice.to_sym][my_choice.to_sym]
+    elsif mode == '2'
+      output = rules[opponent_choice.to_sym].key(my_choice).to_s
+    end
+
+    return output
+  end
+
+  def get_score (my_choice, outcome)
     values = {'rock': 1, 'paper':2, 'scissors':3, 'lose':0, 'tie':3, 'win':6}
-
-    opponent_choice = decode(round[0].to_sym)
-    my_choice = decode(round[1].to_sym)
-
-    outcome = apply_rules(my_choice, opponent_choice)
     score = values[my_choice.to_sym] + values[outcome.to_sym]
+    return score
+  end
+
+  def calculate_round (codes, mode)
+    opponent_code = codes[0]
+    my_code = codes[1]
+    score = nil
+
+    if mode == '1'
+      opponent_choice = decode_1(opponent_code.to_sym)
+      my_choice = decode_1(my_code.to_sym)
+      outcome = apply_rules(my_choice, opponent_choice, mode)
+      score = get_score(my_choice, outcome)
+    elsif mode == '2'
+      opponent_choice = decode_2(opponent_code.to_sym)
+      outcome = decode_2(my_code.to_sym)
+      my_choice = apply_rules(outcome, opponent_choice, mode)
+      score = get_score(my_choice, outcome)
+    end
+
+    score = get_score(my_choice, outcome)
 
     return score
   end
 
-  def calculate_score (guide)
+  def calculate_score (guide, mode)
     score = 0
 
     guide.each do |line|
-      score += calculate_round(line)
+      score += calculate_round(line, mode)
     end
 
     return score
@@ -46,8 +75,12 @@ end
 if __FILE__ == $0    
   day2 = Day02.new
   guide = day2.get_strategy_guide('inputs/realinput02.txt')
-  score = day2.calculate_score(guide)
 
+  score1 = day2.calculate_score(guide, '1')
   print "Part 1:\n"
-  print score.to_s + "\n"
+  print score1.to_s + "\n"
+
+  score2 = day2.calculate_score(guide, '2')
+  print "Part 2:\n"
+  print score2.to_s + "\n"
 end
--- 2022/tests/test02.rb
+++ 2022/tests/test02.rb
@@ -15,7 +15,7 @@ class Part1 < Test::Unit::TestCase
     day2 = Day02.new
 
     column = 'A'
-    choice = day2.decode(column)
+    choice = day2.decode_1(column)
     expected = 'rock'
 
     assert_equal expected, choice
@@ -26,17 +26,33 @@ class Part1 < Test::Unit::TestCase
 
     my_choice = 'rock'
     opponent_choice = 'scissors'
-    outcome = day2.apply_rules(my_choice, opponent_choice)
+    outcome = day2.apply_rules(my_choice, opponent_choice, '1')
     expected = 'win'
 
     assert_equal expected, outcome
+  end
+
+  def test_get_score
+    day2 = Day02.new
+    
+    my_choice1 = 'paper'
+    outcome1 = 'win'
+    expected1 = 8
+    score1 = day2.get_score(my_choice1, outcome1)
+    assert_equal expected1, score1
+
+    my_choice2 = 'rock'
+    outcome2 = 'lose'
+    expected2 = 1
+    score2 = day2.get_score(my_choice2, outcome2)
+    assert_equal expected2, score2
   end
 
   def test_calculate_round
     day2 = Day02.new
     round = ['A', 'Y']
 
-    score = day2.calculate_round(round)
+    score = day2.calculate_round(round, '1')
 
     expected = 8
     assert_equal expected, score
@@ -46,9 +62,55 @@ class Part1 < Test::Unit::TestCase
     day2 = Day02.new
     guide = [['A', 'Y'], ['B', 'X'], ['C', 'Z']]
 
-    score = day2.calculate_score(guide)
+    score = day2.calculate_score(guide, '1')
 
     expected = 15
+    assert_equal expected, score
+  end
+end
+
+class Part2 < Test::Unit::TestCase
+  def test_decode_2
+    day2 = Day02.new
+    column = 'Y'
+    choice = day2.decode_2(column)
+    expected = 'tie'
+
+    assert_equal expected, choice
+  end
+
+  def test_apply_rules_2
+    day2 = Day02.new
+    
+    my_choice = 'tie'
+    opponent_choice = 'scissors'
+    outcome = day2.apply_rules(my_choice, opponent_choice, '2')
+    expected = 'scissors'
+
+    assert_equal expected, outcome
+  end
+
+  def test_calculate_round_2
+    day2 = Day02.new
+
+    round1 = ['A', 'Y']
+    score = day2.calculate_round(round1, '2')
+    expected = 4
+    assert_equal expected, score
+
+    round2 = ['B', 'X']
+    score = day2.calculate_round(round2, '2')
+    expected = 1
+    assert_equal expected, score
+    end
+
+  def test_calculate_score_2
+    day2 = Day02.new
+    guide = [['A', 'Y'], ['B', 'X'], ['C', 'Z']]
+
+    score = day2.calculate_score(guide, '2')
+
+    expected = 12
     assert_equal expected, score
   end
 end
