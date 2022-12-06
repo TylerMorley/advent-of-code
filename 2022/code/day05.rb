c2cd1efb653de90796b23e0f8b4179905c0f54f9
Tyler Morley <TylerPMorley@gmail.com>
2022-12-05 19:17:14 -0600

    day05 part 1

---
+++ 2022/code/day05.rb
@@ -0,0 +1,111 @@
+#!/usr/bin/ruby
+
+class Day05
+  def get_input (filename)
+    lines = File.read(filename).to_s.split("\n\n").map{|line| line = line.split("\n")}
+
+    stacks = Array.new
+    lines[0].pop
+    lines[0].each do |row|
+      stacks.append(refine_stacks(row))
+    end
+    
+    procedures = Array.new
+    lines[1].each do |row|
+      procedures.append(refine_procedure(row))
+    end
+
+    input = [stacks, procedures]
+    return input
+  end
+
+  def refine_crate(crate)
+    output = nil
+    crate.delete(' ')
+    if crate.length > 0
+      output = crate[1]
+    end
+    return output
+  end
+
+  def refine_stacks(row)
+    refined = Array.new
+    row_list = row.chars
+
+    while row_list.length > 3 do
+      entry = row_list.shift 4
+      refined.append(refine_crate(entry))
+    end
+    refined.append(refine_crate(row_list))
+
+    return refined
+  end
+
+  def refine_procedure(raw_procedure)
+    procedure = Array.new
+    words = ['move ', ' from ', ' to ']
+    start_i = 0
+    words.each do |word|
+      if start_i > 0
+        end_i = raw_procedure.index(word) - 1
+        number = raw_procedure[start_i..end_i]
+        procedure.append(number.to_i)
+      end
+      start_i = raw_procedure.index(word) + word.length
+    end
+    end_i = raw_procedure.index(words[-1]) + words[-1].length
+    procedure.append(raw_procedure[end_i..].to_i)
+    return procedure
+  end
+
+  def to_stacks(rows)
+    stacks = rows.transpose
+    output = Array.new
+    stacks.each do |stack|
+      stack.delete(nil)
+      output.append(stack.reverse)
+    end
+    return output
+  end
+
+  def execute_step(stacks, step)
+    quantity = step[0]
+    origin = step[1]-1
+    destination = step[2]-1
+
+    for i in 1..quantity do
+      temp = stacks[origin].pop()
+      stacks[destination].push(temp)
+    end
+    return stacks
+  end
+
+  def execute_procedure(stacks, procedure)
+    index = 1
+    procedure.each do |step|
+      index += 1
+      stacks = execute_step(stacks, step)
+    end
+    return stacks
+  end
+
+  def get_top_crates(stacks)
+    output = ''
+    stacks.each do |stack|
+      output += stack.pop()
+    end
+    return output
+  end
+end
+
+if __FILE__ == $0    
+  day5 = Day05.new
+  input = day5.get_input('inputs/realinput05.txt')
+  stacks = day5.to_stacks(input[0])
+  procedure = input[1]
+
+  moved_stacks = day5.execute_procedure(stacks, procedure)
+  part1 = day5.get_top_crates(moved_stacks)
+  print "Part 1:\n"
+  print part1.to_s + "\n"
+end
---
+++ 2022/tests/test05.rb
@@ -0,0 +1,128 @@
+require 'test/unit'
+require 'code/day05'
+
+class Part1 < Test::Unit::TestCase
+  def test_get_assignments
+    day5 = Day05.new
+
+    assignments = day5.get_input('inputs/testinput05.txt')
+    expected = [[[nil, 'D', nil],
+                 ['N', 'C', nil],
+                 ['Z','M', 'P']
+                ],
+                [[1, 2, 1],
+                 [3, 1, 3],
+                 [2, 2, 1],
+                 [1, 1, 2]
+                ]
+               ]
+
+    assert_equal expected, assignments
+  end
+
+  def test_refine_stacks
+    day5 = Day05.new
+    input = "    [D]    "
+
+    stacks = day5.refine_stacks(input)
+    expected = [nil, 'D', nil]
+    assert_equal expected, stacks
+
+    input2 = "[N] [C]    "
+    stacks = day5.refine_stacks(input2)
+    expected = ['N', 'C', nil]
+    assert_equal expected, stacks
+
+    input3 = "[Z] [M] [P]"
+    stacks = day5.refine_stacks(input3)
+    expected = ['Z', 'M', 'P']
+    assert_equal expected, stacks
+  end
+
+  def test_refine_crate
+    day5 = Day05.new
+
+    input = [" ", " ", " ", "  "]
+    output = day5.refine_crate(input)
+    expected = nil
+    assert_equal expected, output
+
+    input2 = ["[", "D", "]", " "]
+    output = day5.refine_crate(input2)
+    expected = 'D'
+    assert_equal expected, output
+  end
+
+  def test_refine_procedure
+    day5 = Day05.new
+    input = "move 1 from 2 to 1"
+
+    procedure = day5.refine_procedure(input)
+    expected = [1, 2, 1]
+    assert_equal expected, procedure
+
+    input2 = "move 3 from 1 to 3"
+    input3 = "move 2 from 2 to 1"
+    input4 = "move 1 from 1 to 2"
+  end
+
+  def test_to_stacks
+    day5 = Day05.new
+    input = [[nil, 'D', nil],
+             ['N', 'C', nil],
+             ['Z','M', 'P']
+            ]
+    stacks = day5.to_stacks(input)
+    expected = [['Z', 'N'],
+                ['M', 'C', 'D'],
+                ['P']
+               ]
+
+    assert_equal expected, stacks
+  end
+
+  def test_execute_step
+    day5 = Day05.new
+    input = [['Z', 'N'],
+             ['M', 'C', 'D'],
+             ['P']
+            ]
+    step1 = [1, 2, 1]
+    output = day5.execute_step(input, step1)
+    expected = [['Z', 'N', 'D'],
+                ['M', 'C'],
+                ['P']
+               ]
+    assert_equal expected, output
+  end
+
+  def test_execute_procedure
+    day5 = Day05.new
+    stacks = [['Z', 'N'],
+              ['M', 'C', 'D'],
+              ['P']
+             ]
+    procedure = [[1, 2, 1],
+                 [3, 1, 3],
+                 [2, 2, 1],
+                 [1, 1, 2]
+                ]
+    expected  = [['C'],
+                 ['M'],
+                 ['P', 'D', 'N', 'Z']
+                ]
+    output = day5.execute_procedure(stacks, procedure)
+    assert_equal expected, output
+  end
+
+  def test_get_top_crates
+    day5 = Day05.new
+    input = [['C'],
+             ['M'],
+             ['P', 'D', 'N', 'Z']
+            ]
+    expected = 'CMZ'
+    output = day5.get_top_crates(input)
+    assert_equal expected, output
+  end
+end  
