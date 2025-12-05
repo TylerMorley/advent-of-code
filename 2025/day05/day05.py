817d0aaca21d98110c51fbccc19fbdd0ff2ca207
Tyler Morley <TylerPMorley@gmail.com>
2025-12-05 16:30:52 -0600

    Day 5 part 2

--- 2025/day05/day05.py
+++ 2025/day05/day05.py
@@ -23,7 +23,73 @@ def countFreshIngredients(ingredient_ids, ranges):
 
     return fresh_count
 
+def compareRanges(new_range, existing_range):
+        new_low, new_high = new_range
+        existing_low, existing_high = existing_range
+
+        #new range completely within current range
+        if new_low >= existing_low and new_high <= existing_high:
+            return ['subset', None]
+        #new range above current range
+        if new_low > existing_high:
+            return ['disjoint', None]
+        #new range below current range
+        if new_high < existing_low:
+            return ['disjoint', None]
+        #new range overlaps on high side
+        if new_low >= existing_low and new_high > existing_high:
+            message = 'update'
+            update = [existing_low, new_high]
+            return [message, update]
+        #new range overlaps on low side
+        if new_low < existing_low and new_high <= existing_high:
+            overlap = new_high - existing_low
+            message = 'update'
+            update = [new_low, existing_high]
+            return [message, update]
+        #new range completely contains current range
+        if new_low < existing_low and new_high > existing_high:
+            overlap = existing_high - existing_low
+            message = 'update'
+            return [message, new_range]
+        else:
+            print(f'Error')
+            print(f'new_range: {new_range}, existing_range: {existing_range}') 
+
+def buildIdDb(new_range, cur_db):
+    for i in range(len(cur_db)):
+        message, update = compareRanges(new_range, cur_db[i])
+        if message == 'disjoint':
+            continue
+        elif message == 'subset':
+            return cur_db
+        elif message == 'update':
+            updated_range = update
+            temp_db = cur_db[:i] + cur_db[i+1:]
+            updated_db = buildIdDb(updated_range, temp_db)
+            return updated_db
+  
+    cur_db.append(new_range)
+    new_low, new_high = new_range
+    add_to_count = new_high - new_low
+    return cur_db
+
+def countFreshIds(ranges):
+    fresh_id_count = 0
+    id_database = []
+    for cur_range in ranges:
+        id_database = buildIdDb(cur_range, id_database)
+
+    for c_range in id_database:
+        start, end = c_range
+        fresh_id_count += end+1 - start
+
+    return fresh_id_count
+
 if __name__ == '__main__':
     ranges, ingredient_ids = getInputs('input05.txt')
     num_fresh = countFreshIngredients(ingredient_ids, ranges)
     print(f'Part 1: {num_fresh}')
+
+    num_possible_fresh = countFreshIds(ranges)
+    print(f'Part 2: {num_possible_fresh}')
--- 2025/day05/test05.py
+++ 2025/day05/test05.py
@@ -22,3 +22,72 @@ class PartOne(unittest.TestCase):
         ingredient_ids = [1, 5, 8, 11, 17, 32]
         num_fresh = day05.countFreshIngredients(ingredient_ids, ranges)
         self.assertEqual(num_fresh, 3)
+
+class PartTwo(unittest.TestCase):
+    def test_compareRanges(self):
+        existing_range = [3,5]
+        new_range = [10,14]
+        message, updates = day05.compareRanges(new_range, existing_range)
+        self.assertEqual(message, 'disjoint')
+        self.assertIsNone(updates)
+
+        existing_range = [3,10]
+        new_range = [5,9]
+        message, updates = day05.compareRanges(new_range, existing_range)
+        self.assertEqual(message, 'subset')
+        self.assertEqual(updates, None)
+
+        existing_range = [10,14]
+        new_range = [12,18]
+        message, updates = day05.compareRanges(new_range, existing_range)
+        self.assertEqual(message, 'update')
+        self.assertEqual(updates, [10,18])
+
+        existing_range = [10,14]
+        new_range = [7,12]
+        message, updates = day05.compareRanges(new_range, existing_range)
+        self.assertEqual(message, 'update')
+        self.assertEqual(updates, [7,14])
+
+        existing_range = [10,14]
+        new_range = [9,20]
+        message, updates = day05.compareRanges(new_range, existing_range)
+        self.assertEqual(message, 'update')
+        self.assertEqual(updates, [9,20])
+
+    def test_buildIdDb(self):
+        counted = [[3,5]]
+        id_range = [10,14]
+        new_counted = day05.buildIdDb(id_range, counted)
+        self.assertTrue(id_range in new_counted)
+
+        counted = [[3,10]]
+        id_range = [5,9]
+        new_counted = day05.buildIdDb(id_range, counted)
+        self.assertEqual(new_counted, [[3,10]])
+
+        counted = [[3,5], [10,14]]
+        id_range = [12,18]
+        new_counted = day05.buildIdDb(id_range, counted)
+        self.assertEqual(new_counted, [[3,5], [10,18]])
+
+        counted = [[3,5], [10,14]]
+        id_range = [7,12]
+        new_counted = day05.buildIdDb(id_range, counted)
+        self.assertEqual(new_counted, [[3,5], [7,14]])
+
+        counted = [[3,5]]
+        id_range = [2,10]
+        new_counted = day05.buildIdDb(id_range, counted)
+        self.assertEqual(new_counted, [[2,10]])
+
+        counted = [[3,5],[10,14],[16,20]]
+        id_range = [12,18]
+        new_counted = day05.buildIdDb(id_range, counted)
+        self.assertEqual(new_counted, [[3,5],[10,20]])
+
+    def test_countFreshIds(self):
+        ranges = [[3, 5], [10, 14], [16, 20], [12, 18]]
+        fresh_ids = day05.countFreshIds(ranges)
+        self.assertEqual(fresh_ids, 14)
+
