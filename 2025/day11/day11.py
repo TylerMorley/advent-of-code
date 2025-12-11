effd8155c4f85f2e71ce0961cd79fc2367ecddf6
Tyler Morley <TylerPMorley@gmail.com>
2025-12-11 12:38:05 -0600

    Day 11 part 2

--- 2025/day11/day11.py
+++ 2025/day11/day11.py
@@ -1,4 +1,6 @@
 #! usr/bin/env python
+import operator
+import functools
 
 def getInputs(filename):
     with open(filename) as f:
@@ -6,15 +8,52 @@ def getInputs(filename):
         racks = {x[0][:-1]:x[1:] for x in lines}
         return racks
 
-def getPaths(racks, location):
-    if location == 'out':
+def getPaths(racks, location, target):
+    if location == target:
         return 1
+    elif location == 'out':
+        return 0
     else:
-        num_paths = sum([getPaths(racks, x) for x in racks[location]])
+        num_paths = sum([getPaths(racks, x, target) for x in racks[location]])
+            
         return num_paths
+
+def getPathsQuick(racks, location, target, mem):
+    if location+target in mem:
+        return mem[location+target]
+    elif location == target:
+        return 1
+    elif location == 'out':
+        return 0
+    else:
+        num_paths = sum([getPathsQuick(racks, x, target, mem) for x in racks[location]])
+        mem[location+target] = num_paths
+        return num_paths
+
+def getPathsDF(racks):
+    second = third = None
+    if getPaths(racks, 'dac','fft') == 0:
+        second,third = ['fft','dac']
+    elif getPaths(racks,'fft','dac') == 0:
+        second,third = ['dac','fft']
+    else:
+        print(f'ERROR - both middles lead to each other')
+
+    segments = [['svr',second],[second,third],[third,'out']]
+    num_paths = []
+    mem = dict()
+    for segment in segments:
+        start,end = segment
+        num_paths.append(getPathsQuick(racks, start, end, mem))
+
+    return functools.reduce(operator.mul, num_paths)
 
 if __name__ == '__main__':
     racks = getInputs('input11.txt')
     start = 'you'
-    num_paths = getPaths(racks, start)
+    end = 'out'
+    num_paths = getPaths(racks, start, end)
     print(f'Part 1: {num_paths}')
+
+    num_df_paths = getPathsDF(racks)
+    print(f'Part 2: {num_df_paths}')
--- 2025/day11/test11.py
+++ 2025/day11/test11.py
@@ -13,5 +13,36 @@ class PartOne(unittest.TestCase):
                  'fff': ['out'], 'ggg': ['out'], 'hhh': ['ccc', 'fff', 'iii'],
                  'iii': ['out']} 
         start = 'you'
-        paths = day11.getPaths(racks, start)
+        end = 'out'
+        paths = day11.getPaths(racks, start, end)
         self.assertEqual(paths, 5)
+
+class PartTwo(unittest.TestCase):
+    def test_getPathsQuick(self):
+        racks = {'aaa': ['you', 'hhh'], 'you': ['bbb', 'ccc'], 'bbb': ['ddd', 'eee'],
+                 'ccc': ['ddd', 'eee', 'fff'], 'ddd': ['ggg'], 'eee': ['out'],
+                 'fff': ['out'], 'ggg': ['out'], 'hhh': ['ccc', 'fff', 'iii'],
+                 'iii': ['out']} 
+        start = 'you'
+        end = 'out'
+        mem = dict()
+        paths = day11.getPathsQuick(racks, start, end, mem)
+        self.assertEqual(paths, 5)
+
+        racks = {'svr': ['aaa', 'bbb'], 'aaa': ['fft'], 'fft': ['ccc'],
+                 'bbb': ['tty'], 'tty': ['ccc'], 'ccc': ['ddd', 'eee'],
+                 'ddd': ['hub'], 'hub': ['fff'], 'eee': ['dac'], 'dac': ['fff'],
+                 'fff': ['ggg', 'hhh'], 'ggg': ['out'], 'hhh': ['out']}
+        start = 'fft'
+        end = 'dac'
+        mem = dict()
+        paths = day11.getPathsQuick(racks, start, end, mem)
+        self.assertEqual(paths, 1)
+
+    def test_getPathsDF(self):
+        racks = {'svr': ['aaa', 'bbb'], 'aaa': ['fft'], 'fft': ['ccc'],
+                 'bbb': ['tty'], 'tty': ['ccc'], 'ccc': ['ddd', 'eee'],
+                 'ddd': ['hub'], 'hub': ['fff'], 'eee': ['dac'], 'dac': ['fff'],
+                 'fff': ['ggg', 'hhh'], 'ggg': ['out'], 'hhh': ['out']}
+        paths = day11.getPathsDF(racks)
+        self.assertEqual(paths, 2)
